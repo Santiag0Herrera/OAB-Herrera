86127125114e10fdbfc0120f471059d718230e6b
Santiag0Herrera <93556151+Santiag0Herrera@users.noreply.github.com>
2022-03-24 15:52:34 -0300

    Actualización componentes!

--- src/App.js
+++ src/App.js
@@ -14,18 +14,13 @@ function App() {
         <h1>Nuestros Productos</h1>
 
         <div className="catalog">
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
+          <Card title='Cloro' description='Vidon de cloro 10lts para piletas.' price='1500'/>
+
+          <Card title='Barrefondo' description='Limpiador de fondo para piletas de lona limpia facil ecologico' price='3000'/>
+
+          <Card title='Vino' description='Bebida hecha de uva, mediante la fermentación alcohólica.' price='1000'/>
+
+          <Card title='Tecla de Luz' description='dispositivo que  desvia el curso de corriente eléctrica.' price='500'/>          
         </div>
       </div>
 
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import './Card.css'
-function card(){
-    return (
-    <div className='Articulos'>
-        <img src='https://i0.wp.com/brochure3d.com/wp-content/plugins/elementor/assets/images/placeholder.png?w=750&ssl=1'></img>
-        <div className='contenidoArticulos'>
-            <h3>Articulo</h3>
-            <h4>Descripcion del articulo</h4>
-            <h4>$3500</h4>
-            <button>Agregar al Carrito</button>
+
+export default function card(props){
+    return(
+        <div className='Articulos'>
+            <h2>{props.title}</h2>
+            <img src='https://www.educaciontrespuntocero.com/wp-content/uploads/2019/02/girasoles-978x652.jpg'></img>
+            <div className='contenidoArticulos'>
+                <h3>{props.description}</h3>
+                <h3>${props.price}</h3>
+                <button>Comprar</button>
+            </div>
         </div>
-    </div>
     )
 }
-export default card;
