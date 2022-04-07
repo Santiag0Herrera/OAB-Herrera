dd4ce07076269cde648aec6132090e89d0ecebba
Santiag0Herrera <93556151+Santiag0Herrera@users.noreply.github.com>
2022-04-07 19:59:09 -0300

    .

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,31 +1,31 @@
-// import React, {useState, useEffect} from "react";
-// import ItemDetail from '../ItemDetail/ItemDetail';
-// import Catalog from "../../Catalog";
+import React, {useState, useEffect} from "react";
+import ItemDetail from '../ItemDetail/ItemDetail';
+import Catalog from "../../Catalog";
 
-// const ItemDetailContainer = () => {
+const ItemDetailContainer = () => {
 
-//     const [productsInfo, setProductsInfo] = useState([]) //useState([]) = array vacio
-//     const getProducts = () => {
-//             return new Promise ((resolve, reject) => {
-//                 setTimeout( ()=>{ return resolve(Catalog);} , 2000);
-//         });
-//     }
-//     useEffect(()=>{
-//         getProducts().then((data)=>{
-//             setProductsInfo(data);
-//         })
-//     });
+    const [productsInfo, setProductsInfo] = useState([]) //useState([]) = array vacio
+    const getProducts = () => {
+            return new Promise ((resolve, reject) => {
+                setTimeout( ()=>{ return resolve(Catalog);} , 2000);
+        });
+    }
+    useEffect(()=>{
+        getProducts().then((data)=>{
+            setProductsInfo(data);
+        })
+    });
 
-//     return(
-//         <div className="ProductDetailList">
-//             {productsInfo.map((productInfo) => {
-//                 const {id} = productInfo;
-//                 return(
-//                     <ItemDetail data={productInfo} key={id}/>
-//                 )
-//             })}
-//         </div>
-//     )
-// }
+    return(
+        <div className="ProductDetailList">
+            {productsInfo.map((productInfo) => {
+                const {id} = productInfo;
+                return(
+                    <ItemDetail data={productInfo} key={id}/>
+                )
+            })}
+        </div>
+    )
+}
 
-// export default ItemDetailContainer;
+export default ItemDetailContainer;
